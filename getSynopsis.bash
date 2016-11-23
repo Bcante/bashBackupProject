f80162b820cfd8c3e4b4325f230e9208a222aed0
Benoît Cante <cantebenoit@hotmail.com>
2016-11-23 21:56:43 +0100

    Version basique du curl & wget pour récupérer tous les fichiers .gpg

--- getSynopsis.bash
+++ getSynopsis.bash
@@ -1,1 +1,14 @@
 #!/bin/bash
+#Pour chaque lien, y aller
+curl 'https://daenerys.xplod.fr/synopsis.php' | grep -e '"synopsis.php' | grep -E '<a.*>(.*)</a>' > curlRes
+regex="s=([0-9]+).*e=([0-9]+).*Episode\s[0-9]+:\s(.+)<\/a>"
+
+while read -u 10 p; do
+	if [[ $p =~ $regex ]] ; then
+		SAISON="${BASH_REMATCH[1]}"		
+		EPISODE="${BASH_REMATCH[2]}"
+		wget "https://daenerys.xplod.fr/supsyn.php?e=$EPISODE&s=$SAISON" -O 'S'$SAISON'E'$EPISODE
+	fi
+done 10<curlRes
+
+
