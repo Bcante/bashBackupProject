3e9156268fffee606be8ab7ed9362266ea38fb9e
Benoît Cante <cantebenoit@hotmail.com>
2016-11-24 22:00:57 +0100

    Amélioration de la récupération des gpg et txt: Les fichiers sont maintenant envoyés dans le home de l'utilisateur(si j'ai bien compris le sujet)

--- getSynopsis.bash
+++ getSynopsis.bash
@@ -2,12 +2,17 @@
 curl 'https://daenerys.xplod.fr/synopsis.php' | grep -e '"synopsis.php' | grep -E '<a.*>(.*)</a>' > curlRes
 regex="s=([0-9]+).*e=([0-9]+).*Episode\s[0-9]+:\s(.+)<\/a>"
 
+if [ ! -d "/home/$USER/got" ]; then
+	mkdir "/home/$USER/got"			
+fi
+
 while read -u 10 p; do
 	if [[ $p =~ $regex ]] ; then
 		SAISON="${BASH_REMATCH[1]}"		
 		EPISODE="${BASH_REMATCH[2]}"
 		#Récupération PGP		
-		wget "https://daenerys.xplod.fr/supsyn.php?e=$EPISODE&s=$SAISON" -O 'PGP_S'$SAISON'E'$EPISODE
+		wget "https://daenerys.xplod.fr/supsyn.php?e=$EPISODE&s=$SAISON" -O "/home/$USER/got/"'PGP_S'$SAISON'E'$EPISODE -P "/home/$USER/got/"
+
 		#Récupération synopsis
 		curl "https://daenerys.xplod.fr/synopsis.php?s=$SAISON&e=$EPISODE" | grep -E '^([a-zA-Z].*)<|<p class="left-align light">(.*)<' > curlRes2
 		REGEXSYNO="^([a-zA-Z].*)<|<p class=\"left-align light\">(.*)<"
@@ -15,9 +20,14 @@ while read -u 10 p; do
 			if [[ $d =~ $REGEXSYNO ]] ; then
 				SYNO1="${BASH_REMATCH[1]}"		
 				SYNO2="${BASH_REMATCH[2]}"
-				touch 'RESUME_S'$SAISON'E'$EPISODE
-				echo $SYNO1>>'RESUME_S'$SAISON'E'$EPISODE
-				echo $SYNO2>>'RESUME_S'$SAISON'E'$EPISODE
+				
+				FILETMP='Saison '$SAISON' Episode '$EPISODE'.txt'
+				if [ -f "$FILETMP" ]; then				
+					rm $FILETMP		
+				fi
+				touch "/home/$USER/got/$FILETMP"
+				echo "$SYNO1">"/home/$USER/got/$FILETMP"
+				echo "$SYNO2">>"/home/$USER/got/$FILETMP"
 			fi		
 		done 10<curlRes2
 	fi
