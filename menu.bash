c178656fbac35e148995d0723667968be2111783
Benoît Cante <cantebenoit@hotmail.com>
2016-12-10 18:19:24 +0100

    Amélioration interface graphique

--- menu.bash
+++ menu.bash
@@ -1,35 +1,64 @@
 #!/bin/bash
 
-#Improtation des fichiers bashs contenant les fonctions qui nous intéressent
+#Imporation des fichiers bashs contenant les fonctions qui nous intéressent
 source uploadBackup.bash
 source getSynopsis.bash
 
 # $1 : L'option que l'utilisateur a rentré
-function aiguillage () {
+function aiguillageMainMenu () {
 	local choix=$1
 	local message=""
 	case $choix in
-	1)
-	  message="Fonctions Maxime."
-	  dialog --msgbox "$message" 0 0
-	  ;;
-	2)
-	  upMyFile -q
-	  ;;
-	3)
-	  getMyFile
-  	  ;;
-  	4)
-	  getSyno  
-	  ;;
-	0)
-	  quit=1
-	  ;;  
-	"")
-	  quit=1
-	  ;;
+		1)
+		  message="Fonctions Maxime."
+		  dialog --msgbox "$message" 0 0
+		  ;;
+		2)
+		  upMyFile -q
+		  ;;
+		3)
+		  getMyFile
+	  	  ;;
+	  	4)
+		  getSyno  
+		  ;;
+		5)
+		  parametrage
+		  ;;
+		0)
+		  quit=1
+		  ;;  
+
+		#Quand l'utilisateur appuie sur "annuler"
+		"")
+		  quit=1
+		  ;;
 	esac
 }
+
+#1) Permet de changer les fichier/dossiers a svg
+#2) Permet de spécifier le dossier de sortie
+function parametrage {
+	local choixParam=$(dialog --stdout --title "Menu principal" --menu "Menu" 0 0 0 \
+		"0" "Retour" \
+		"1" "Éditer le fichier contenant les dossiers à sauvegarder" \
+		"2" "Spécifier un nouveau dossier de sortie")
+	case $choixParam in
+		1)
+		  nano confFile
+		  ;;
+		2)
+		  local newDir=$(dialog --title --stdout "Nouveau dir de sortie" --dselect /home/$USER/ 0 0)
+		  #Reste à modifier la ligne correspondante dans le fichier de conf...
+		  ;;
+		*)
+		 ;;
+	esac
+	
+}
+
+
+
 
 while getopts "q" opt; do
   case $opt in
@@ -53,8 +82,9 @@ while [ $quit -eq 0 ]; do
 		"1" "Faire un Backup" \
 		"2" "Mettre en ligne un backup" \
 		"3" "Télécharger un backup" \
-		"4" "Télécharger les synopsis")
-	aiguillage $choix
+		"4" "Télécharger les synopsis" \
+		"5" "Paramétres")
+	aiguillageMainMenu $choix
 done
 echo "Au revoir"
 
