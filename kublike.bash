757807efe90af51c00095badb6d51af29b6c5aeb
Bleacks <bleacks@MSI-Debian>
2016-12-12 15:43:34 +0100

    Ajout de l'interface graphique et patch de la backup

--- kublike.bash
+++ kublike.bash
@@ -8,70 +8,84 @@
 ## Définition des fonctions du programme ##
 ###########################################
 
+# Fonction d'affichage des erreurs
+# Params : 
+	# $1 : Message d'erreur
+	# $2 : exit code
+logger () {
+	# if (logger_is_enabled)
+		# log_to_file
+	echo $#
+	if [ $# -ge 1 ]; then
+		if ! [ -z "$1" ]; then 
+			dialog --title "Une erreur à été rencontrée" --msgbox "${1}" 0 0
+		fi
+	else
+		logger "Logger called but no params found" 1
+	fi
+	if [ $# -eq 2 ]; then
+		if ! [ -z "$2" ]; then
+			exit $2
+		fi
+	fi
+}
+
 # Vérification des paramètres de lancement
 function verifyParams {
 	if [ $# -gt 0 ]; then
 		for (( i=0; i<=$#; i+=1 ))
 		do
-			index=$((i+1))
+			local index=$((i+1))
 			if [ ${!i} = "--conf" ];then
 				if [ $index -gt $# ]; then
-					echo "Usage --conf <config file>"
+					logger "Usage --conf <config file>"
 				else
 					if [ -f ${!index} ]; then
 						if [ -r ${!index} ]; then
-							echo "Conf file found ${!index}"
+							# echo "Conf file found ${!index}"
 							conf=${!index}
 						else
-							echo "File not readable"
-							exit 2
+							logger "File not readable" 2
 						fi
 					else
-						echo "File not found"
-						exit 1
+						logger "File not found" 1
 					fi
 				fi
 			elif [ ${!i} = "--backupdir" ]; then
 				if [ $index -gt $# ]; then
-					echo "Usage --backupdir <directory>"
+					logger "Usage --backupdir <directory>"
 				else
 					if [ -d ${!index} ]; then
 						if [ -r ${!index} ]; then
-							echo "Backup Directory found ${!index}"
+							# echo "Backup Directory found ${!index}"
 							backupdir=${!index}
 						else
-							echo "Directory not readable"
-							exit 2
+							logger "Directory not readable" 2
 						fi
 					else
-						echo "Backup Directory not found"
-						exit 1
+						logger "Backup Directory not found" 1
 					fi
 				fi
 			fi
 		done
-		if ! [ -z $error ]; then
-			echo "$error";
-			#TODO : Récupérer code d'erreur du bloc for précédent
-		fi
 	else
+		local error=""
 		if [ -f $conf ]; then
 			if ! [ -r $conf ]; then
-				error="Config File not readable"$'\n'
+				error="Config File not readable"$'\n'" "
 			fi
 		else
-			error="$error File not found"$'\n'
+			error="${error}File not found"$'\n'
 		fi
 		if [ -d $backupdir ]; then
 			if ! [ -r $backupdir ]; then
-				error="$error BackupDir not readable"$'\n'
+				error="${error}BackupDir not readable"$'\n'" "
 			fi
 		else
-			error="$error BackupDir not found"$'\n'
+			error="${error} BackupDir not found"$'\n'" "
 		fi
 		if ! [ -z "$error" ]; then
-			echo "$error"
-			exit 1
+			logger "$error" 1
 		fi
 	fi
 }
@@ -79,54 +93,66 @@ function verifyParams {
 # Lecture des chemins surveillés
 function readPaths {
 	# TODO : rename les var files file en générique
-	files=$(cat $conf)
-	found=""
+	local found=""
+	local error=""
 	# Lecture du fichier contenant les différents chemins à sauvegarder
-	for file in $files; do
-		if [ -f $file ] || [ -d $file ]; then
-			if [ -r $file ]; then
-				found=${found}"\n"$file
+	while read -r line; do
+		if [ -f $line ] || [ -d $line ]; then
+			if [ -r $line ]; then
+				if [ -z $found ]; then
+					found=$line
+				else
+					found="${found} ${line}"
+				fi
 			else
-				error=${error}"\nProblème de droit d'accès : "$file
+				error=${error}"\nProblème de droit d'accès : "$line
 			fi
 		else
-			if ! [ -d $file ]; then
-				error=${error}"\nFichier inexistant : "$file
+			if ! [ -d $line ]; then
+				error=${error}"\nFichier inexistant : "$line
 			fi
 		fi
-	done;
-	if ! [ -z "$error" ]; then
-		echo -e "$error"
-		# TODO : choisir un exit code
-		exit 4;
+	done <<< "$conf";
+	if [ -z $found ]; then
+		logger "Aucun fichier n'a été trouvé" 1
+	else	
+		# TODO Faire fonctionner en ajoutant les synopsis de got aux archives
+		doTheTar "$found"
 	fi
-
-	echo "----------------"
-	echo -e "$found"
-	echo "----------------"
+	if ! [ -z "$error" ]; then
+		dialog --title "Tous les fichiers n'ont pas étés trouvés, continuer ?" --yesno "$error" 0 0
+		local answer=$?
+		if [ $answer -eq  0 ]; then
+			exit 4
+		fi
+	fi
 }
 
 # Création du nom de la sauvegarde
 function chooseBackupName {
-	date=$(date +%Y-%m-%d_%H-%M)
-	name=${backupdir}${date}.tar.gz
+	local date=$(date +%Y-%m-%d_%H-%M)
+	local name=${backupdir}${date}.tar.gz
 	
 	# Différence entre deux backups effectuées au même moment
 	if [ -f $name ]; then
 		count=$(find ${backupdir}${date}* -maxdepth 1 -type f | wc -l)
 		name=${backupdir}${date}_${count}.tar.gz
 	fi
+	echo $name
 }
 
 # Création de la sauvegarde
 function doTheTar {
-	tar -zcvf $name --files-from $found
+	# TODO : faire une redirection des erreurs dans une variable pour l'afficher dans le logger
+	local error="$(tar -zcvf "$(chooseBackupName)" --files-from $1 -C / home/${USER}/Got 2>&1 > /dev//null)"
+	if ! [ -z "$error" ]; then
+		logger "$error"
+	fi
 }
 
 # Maintien du nombre de backup a 100 maximum
 function clearOldBackups {
-	backupCount=`ls $backupdir | grep -E "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}(_[0-9]+)?\.tar\.gz" | wc -l`
-
+	local backupCount=`ls $backupdir | grep -E "[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}(_[0-9]+)?\.tar\.gz" | wc -l`
 	if [ $backupCount -ge 100 ]; then
 		file=`ls -tr backups | head -n 1`
 		`rm ${backupdir}$file`
@@ -159,10 +185,16 @@ function decrypt {
 	gpg2 --passphrase $3 --decrypt $1
 }
 
+function doTheBackup {
+	verifyParams "$@"
+	chooseBackupName
+	readPaths
+	clearOldBackups
+}
+
 ###############################
 ## Préparation des variables ##
 ###############################
-error=""
 conf="backup.conf"
 fichierConf="kublike.conf"
 backupdir="backups/"
@@ -170,9 +202,9 @@ backupdir="backups/"
 ############################
 ## Execution du programme ##
 ############################
-verifyParams "$@"
-readPaths
-chooseBackupName
-doTheTar
-clearOldBackups
-encrypt $name `cat $fichierConf`
+doTheBackup "$@"
+#encrypt $name `cat $fichierConf`
+
+# TODO : penser à utiliser des local
+# TODO : fonction magique
+# TODO : tester avec des chemins absolus
